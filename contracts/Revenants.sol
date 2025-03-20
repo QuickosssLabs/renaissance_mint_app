155851b59f2db39b523075f77273e45a1b9a38e3
QC <129374833+QuickosssLabs@users.noreply.github.com>
2025-03-20 21:38:22 +0100

    Update Revenants.sol

--- contracts/Revenants.sol
+++ contracts/Revenants.sol
@@ -5,10 +5,11 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract Revenants is ERC721, Ownable, ReentrancyGuard {
+contract Revenants is ERC721, Ownable, ReentrancyGuard, Pausable {
     using Counters for Counters.Counter;
     using Strings for uint256;
     Counters.Counter private _tokenIds;
@@ -23,13 +24,20 @@ contract Revenants is ERC721, Ownable, ReentrancyGuard {
     mapping(address => uint256) public mintedPerWallet;
     
     event NFTMinted(address indexed to, uint256 tokenId);
+    event ContractPaused(address indexed account);
+    event ContractUnpaused(address indexed account);
     
-    constructor(address _renaissanceContract) ERC721("(Re:)venants", "RVNT") Ownable(msg.sender) {
+    constructor(address _renaissanceContract, bool _paused) ERC721("(re:)venants", "RVNT") Ownable(msg.sender) {
         renaissanceContract = IERC1155(_renaissanceContract);
         _baseTokenURI = "ipfs://bafybeiahj6xqgyead7fjhgra6rcsafvxnufqfp5yyjbwlh4ryu22asfkbm/";
+        
+        if (_paused) {
+            _pause();
+            emit ContractPaused(msg.sender);
+        }
     }
     
-    function mint(uint256 quantity) external nonReentrant {
+    function mint(uint256 quantity) external nonReentrant whenNotPaused {
         require(quantity > 0, "Invalid quantity");
         require(_tokenIds.current() + quantity <= MAX_SUPPLY, "Would exceed max supply");
         
@@ -55,7 +63,7 @@ contract Revenants is ERC721, Ownable, ReentrancyGuard {
     }
     
     // Function for owner to mint multiple NFTs at once
-    function ownerMint(address to, uint256 quantity) external onlyOwner {
+    function ownerMint(address to, uint256 quantity) external onlyOwner whenNotPaused {
         require(quantity > 0, "Invalid quantity");
         require(_tokenIds.current() + quantity <= MAX_SUPPLY, "Would exceed max supply");
         
@@ -67,7 +75,18 @@ contract Revenants is ERC721, Ownable, ReentrancyGuard {
             emit NFTMinted(to, newTokenId);
         }
     }
-    
+
+    // Pause functions
+    function pause() external onlyOwner {
+        _pause();
+        emit ContractPaused(msg.sender);
+    }
+
+    function unpause() external onlyOwner {
+        _unpause();
+        emit ContractUnpaused(msg.sender);
+    }
+
     function hasCompleteSet(address wallet) public view returns (bool) {
         // Check if user has at least one of each token
         for (uint256 i = START_TOKEN_ID; i <= END_TOKEN_ID; i++) {
